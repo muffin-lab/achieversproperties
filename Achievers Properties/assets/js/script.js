c0f585fa386348cec3aafa0578c7e64b7535619d
Kiran Kannan <62441262+muffin-lab@users.noreply.github.com>
2023-05-13 18:22:26 +0530

    Initial Commit
    Full project upload

---
+++ Achievers Properties/assets/js/script.js
@@ -0,0 +1,110 @@
+"use strict";
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [overlay, navCloseBtn, navOpenBtn];
+
+/**
+ * close navbar when click on any navbar link
+ */
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navElemArr.push(navbarLinks[i]);
+}
+
+/**
+ * addd event on all elements for toggling navbar
+ */
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+  });
+}
+
+/**
+ * header active state
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 400
+    ? header.classList.add("active")
+    : header.classList.remove("active");
+});
+
+("use strict");
+var testim = document.getElementById("testim"),
+  testimDots = Array.prototype.slice.call(
+    document.getElementById("testim-dots").children
+  ),
+  testimContent = Array.prototype.slice.call(
+    document.getElementById("testim-content").children
+  ),
+  testimleftArrow = document.getElementById("left-arrow"),
+  testimRightArrow = document.getElementById("right-arrow"),
+  testimSpeed = 4500,
+  currentSlide = 0,
+  currentActive = 0,
+  testimTimer;
+// coding with nick
+window.onload = function () {
+  // Testim Script
+  function playSlide(slide) {
+    for (var k = 0; k < testimDots.length; k++) {
+      testimContent[k].classList.remove("active");
+      testimContent[k].classList.remove("inactive");
+      testimDots[k].classList.remove("active");
+    }
+    if (slide < 0) {
+      slide = currentSlide = testimContent.length - 1;
+    }
+    if (slide > testimContent.length - 1) {
+      slide = currentSlide = 0;
+    }
+    if (currentActive != currentSlide) {
+      testimContent[currentActive].classList.add("inactive");
+    }
+    testimContent[slide].classList.add("active");
+    testimDots[slide].classList.add("active");
+
+    currentActive = currentSlide;
+
+    clearTimeout(testimTimer);
+    testimTimer = setTimeout(function () {
+      playSlide((currentSlide += 1));
+    }, testimSpeed);
+  }
+  // coding with nick
+  testimleftArrow.addEventListener("click", function () {
+    playSlide((currentSlide -= 1));
+  });
+  testimRightArrow.addEventListener("click", function () {
+    playSlide((currentSlide += 1));
+  });
+
+  for (var l = 0; l < testimDots.length; l++) {
+    testimDots[l].addEventListener("click", function () {
+      playSlide((currentSlide = testimDots.indexOf(this)));
+    });
+  }
+  playSlide(currentSlide);
+};
